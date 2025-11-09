e69572074ab460ef9257a77e325c2ccdc2a3330c
chetandhunna1 <chetan.dhunna1@gmail.com>
2025-11-09 22:13:12 +1100

    New code added

---
+++ src/test/java/pages/Webshop/Webshop.java
@@ -0,0 +1,62 @@
+package pages.Webshop;
+
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.PageObject;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.PageFactory;
+
+public class Webshop extends PageObject {
+
+
+    public String url = "https://demowebshop.tricentis.com/";
+    public String userEmail = "myemail19@email.com";
+    public String userPassword = "Password@123";
+
+    @FindBy(xpath = "//*[@class='ico-login']")
+     WebElement login_link;
+
+    @FindBy(xpath  = "//a[@href='/logout']")
+    WebElement logout_link;
+
+    @FindBy(xpath  = "//a[@href='/register']")
+    WebElement register_link;
+
+    @FindBy(xpath  = "//span[@class='cart-label']")
+    WebElement shopping_cart;
+
+    @FindBy(xpath  = "//input[@class='email']")
+    WebElement email;
+
+    @FindBy(xpath  = "//input[@class='password']")
+    WebElement password;
+
+
+    @FindBy(xpath  = "//input[@class='button-1 login-button']")
+    WebElement login_Button;
+
+
+
+public void openPage() throws InterruptedException {
+    openUrl(url);
+
+
+}
+
+public void loginLink()
+{
+    login_link.click();
+
+
+}
+public void enterCreds()
+{
+    email.sendKeys(userEmail);
+    password.sendKeys(userPassword);
+    login_Button.click();
+
+}
+
+
+
+}
